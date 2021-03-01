31215789581745a98fd6622bf07610dab4c2a3af
Nicolas POYET <nicolas.poyet@gmail.com>
2021-03-01 17:39:18 +0100

    search page in progress with working api calls

--- reactapp/src/screens/SearchScreen.js
+++ reactapp/src/screens/SearchScreen.js
@@ -1,19 +1,95 @@
-// https://books.googleapis.com/books/v1/volumes?q=andr%C3%A9%20compte-sponville&maxResults=40&orderBy=relevance&key=[YOUR_API_KEY]
-// API_key: "AIzaSyAIdljyRBhHojVGur6_xhEi1fdSKyb-rUE"
+import React, {useState} from 'react';
+import { Card, Menu, Dropdown, Input, Row, Col, Button} from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+// import Navbar from '../components/Navbar.js'
+// cas 0
+// image pas dispo
+// pagination
+// titre
+// tri nouveautés
 
-import React, {useState, useEffect} from 'react';
-import { Card, Icon, Modal, Input} from 'antd';
-// import Navbar from './components/Navbar.js'
-import {connect} from 'react-redux';
+const { Search } = Input;
+const { Meta } = Card;
 
+export default function SearchScreen() {
 
-export default function SearchScreen(props) {
+    const [result, setResult] = useState([]);
+    const [sortingMethod, setSortingMethod] = useState("relevance");
 
-    const onSearch = value => console.log(value);
+    const menu = (
+        <Menu onClick={handleMenuClick}>
+          <Menu.Item key="1">Pertinence</Menu.Item>
+          <Menu.Item key="2">Date de publication</Menu.Item>
+        </Menu>
+      );
 
-<View>
-<Text>Votre Recherche</Text>
-<Input placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+    function handleMenuClick(e) {
+        if (e.key === "2") {
+            setSortingMethod("newest");
+            bookSearchApi();
+        }
+      }
 
-</View>
+    const bookSearchApi = async(query) => {
+        const data = await fetch(`https://books.googleapis.com/books/v1/volumes?q=${query}&maxResults=40&langRestrict=fr&orderBy=${sortingMethod}&apiKey=AIzaSyBDzd4vX9LAeML4Hsway4y63xn2ReLuPOc`)
+        const body = await data.json();
+        setResult(body.items);
+        console.log(body.items);
+      };  
+
+return (
+<div style={{ width:"80%", margin:"auto"}}>
+    <Row className="searchInput"  align="middle" justify="center">
+        <Col xs={24} md={6} style={styles.textAlign}>
+            <div >Votre Recherche</div>
+        </Col>
+        <Col xs={24} md={12} >
+            <Search size="large" placeholder="Chercher un auteur, titre, ISBN, ..." onSearch={(e) => bookSearchApi(e)} />
+        </Col>
+    </Row>
+    <Row align="middle" justify="space-between" style={{marginTop:20}}>
+        <div>{result.length} Livres trouvés</div>
+        <Dropdown overlay={menu}>
+            <Button style={{ marginLeft: 8 }}>
+                Trier par <DownOutlined  />
+            </Button>
+        </Dropdown>
+    </Row>
+    <div style={{display:'flex', flexWrap:"wrap" ,justifyContent: "center"}}>
+              {result.map((book,i) => (
+                <div key={i} style={{display:'flex',justifyContent:'center'}}>
+    <Card 
+        style={{ 
+            width: 100,
+            margin:'15px', 
+            display:'flex',
+            flexDirection: 'column',
+            justifyContent:'space-between'    
+        }} 
+        bodyStyle={{ padding: 0 }}
+        cover={
+            <img
+                alt={book.volumeInfo.title}
+                src={!book.volumeInfo.imageLinks ? "../logo192.png" : book.volumeInfo.imageLinks.thumbnail} 
+            />
+            }
+        // actions={}
+        >
+        <Meta
+        title={book.volumeInfo.title}
+        style={{fontSize:12}}
+        />
+    </Card>
+    </div>
+              ))}
+    </div>
+</div>
+
+)}
+
+let styles ={ 
+    textAlign: {
+        textAlign: "right",
+        padding:"10px"
+    }
 }
