23047c4edffd72212cc8e505e454739bed3a6f48
Cel <celineregnaud@gmail.com>
2021-03-01 16:08:03 +0100

    clean navbar for others to try

--- reactapp/src/components/Navbar.js
+++ reactapp/src/components/Navbar.js
@@ -1,15 +1,17 @@
+// To do : 
+// mettre à jour les liens de navigation une fois tous les écrans créés
+// Importer le user token, son nom, sa photo pour l'avatar
+// Gérer le log out
+
 import React, { useState } from 'react';
 import logo from '../assets/bookin.png';
 import './Navbar.css';
-
 // ReactStrap
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem,
-  NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem
-} from 'reactstrap';
-
+  NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 // Ant Design
 import { Image, Avatar } from 'antd';
-import { BulbOutlined, HeartOutlined, SearchOutlined, BookOutlined, LoginOutlined, LogoutOutlined, SettingOutlined, UserOutlined, MenuOutlined } from '@ant-design/icons';
+import { BulbOutlined, HeartOutlined, SearchOutlined, BookOutlined, LoginOutlined, LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
 
 function NavigationBar(props) {
   // Large menu
@@ -19,51 +21,70 @@ function NavigationBar(props) {
   const [collapsed, setCollapsed] = useState(true);
   const toggleNavbar = () => setCollapsed(!collapsed);
 
+  // user token
+  // passer en true et en false pour voir les différents cas
+  // (à modifier une fois stocké dans redux)
+  const [token, setToken] = useState(true);
+
   return(
-  <div>
-    {/* Large Menu, User is connected */}
+    <div>
+    {/* Large Menu */}
     <div className="d-none d-sm-none d-md-inline">
       <Navbar expand="md" className="menu-nav">
         <NavbarBrand href="/"><Image src={logo} alt="logo Bookin" width={90}/></NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar className="right-menu">
           <Nav navbar>
-            <NavItem><NavLink href="/" className="menu-nav-item"><SearchOutlined className="menu-nav-icon" /> Rechercher</NavLink></NavItem>
-            <NavItem ><NavLink href="/" className="menu-nav-item"><BulbOutlined className="menu-nav-icon"/> Suggestions</NavLink></NavItem>
-            <NavItem ><NavLink href="/" className="menu-nav-item"><BookOutlined className="menu-nav-icon"/> Bibliothèque</NavLink></NavItem>
+            <NavItem><NavLink href="/search" className="menu-nav-item"><SearchOutlined className="menu-nav-icon" /> Rechercher</NavLink></NavItem>
+            <NavItem ><NavLink href="/result" className="menu-nav-item"><BulbOutlined className="menu-nav-icon"/> Suggestions</NavLink></NavItem>
+            <NavItem ><NavLink href="/library" className="menu-nav-item"><BookOutlined className="menu-nav-icon"/> Bibliothèque</NavLink></NavItem>
             <NavItem ><NavLink href="/" className="menu-nav-item"><HeartOutlined className="menu-nav-icon"/> A lire</NavLink></NavItem>
-            <UncontrolledDropdown nav inNavbar>
+            { token ? 
+              <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav>
-              <Avatar icon={<UserOutlined/>}/>
+                <Avatar className="menu-nav-avatar" icon={<UserOutlined/>}/>
               </DropdownToggle>
               <DropdownMenu right>
-                <DropdownItem className="menu-nav-item">Modifier mon compte</DropdownItem>
-                <DropdownItem className="menu-nav-item">Me déconnecter</DropdownItem>
+                <DropdownItem href='/' className="menu-nav-item">Modifier compte</DropdownItem>
+                <DropdownItem href='/' className="menu-nav-item">Déconnexion</DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
+            :
+            <NavItem><NavLink href="/search" className="menu-nav-item"><LoginOutlined className="menu-nav-icon" /> Connexion</NavLink></NavItem>
+            }
           </Nav>
         </Collapse>
       </Navbar>
       </div>
-    {/* Small Menu, User is connected */}  
-    <div className="d-md-none">
-      <Navbar color="faded" light>
+    {/* Small Menu */}  
+    <div className="d-md-none ">
+      <Navbar light>
       <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <NavbarBrand href="/" className="mr-auto"><Image src={logo} alt="logo Bookin" width={90}/></NavbarBrand>
-        <NavbarBrand href="/" className="mr-auto"><SearchOutlined /></NavbarBrand>
-        <NavbarBrand href="/" className="mr-auto"><UserOutlined/></NavbarBrand>
-        <Collapse isOpen={!collapsed} navbar>
+      <NavbarBrand href="/" className="mr-auto"><Image src={logo} alt="logo Bookin" width={90}/></NavbarBrand>
+      <Navbar light className="menu-nav-header-xs">
+      <NavbarBrand href="/search"><SearchOutlined className="mr-auto menu-nav-header-icon-xs"/></NavbarBrand>
+      <NavbarBrand href="/"><UserOutlined className="mr-auto menu-nav-header-icon-xs"/></NavbarBrand>
+      </Navbar>
+      <Collapse isOpen={!collapsed} navbar>
           <Nav navbar className="left-menu-xs">
-            <NavItem><NavLink href="/" className="menu-nav-item-xs"><BulbOutlined className="menu-nav-icon-xs"/> Suggestions</NavLink></NavItem>
-            <NavItem><NavLink href="/" className="menu-nav-item-xs"><BookOutlined className="menu-nav-icon-xs" /> Bibliothèque</NavLink></NavItem>
+            <NavItem><NavLink href="/result" className="menu-nav-item-xs"><BulbOutlined className="menu-nav-icon-xs"/> Suggestions</NavLink></NavItem>
+            <NavItem><NavLink href="/library" className="menu-nav-item-xs"><BookOutlined className="menu-nav-icon-xs" /> Bibliothèque</NavLink></NavItem>
             <NavItem><NavLink href="/" className="menu-nav-item-xs"><HeartOutlined className="menu-nav-icon-xs" /> A lire</NavLink></NavItem>
-            <NavItem><NavLink href="/" className="menu-nav-item-xs"><SettingOutlined className="menu-nav-icon-xs" /> Modifier mon compte</NavLink></NavItem>
-            <NavItem><NavLink href="/" className="menu-nav-item-xs"><LogoutOutlined className="menu-nav-icon-xs" /> Me déconnecter</NavLink></NavItem>
+            {token ? 
+            <div>
+             <NavItem><NavLink href="/" className="menu-nav-item-xs"><SettingOutlined className="menu-nav-icon-xs" /> Modifier compte</NavLink></NavItem>
+             <NavItem><NavLink href="/" className="menu-nav-item-xs"><LogoutOutlined className="menu-nav-icon-xs" /> Déconnexion</NavLink></NavItem>
+            </div>
+            :
+            <div></div>
+            }
+           
           </Nav>
-        </Collapse>
+      </Collapse>
       </Navbar>
     </div>
-    </div>);
+    </div>
+);
 }
 export default NavigationBar;
 
