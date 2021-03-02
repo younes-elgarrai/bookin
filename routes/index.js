58843d50edd5028230d7e531ff0543160577d632
Cel <celineregnaud@gmail.com>
2021-03-02 10:56:48 +0100

    database creation and connection

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const UsersModel = require('../models/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
