58dbae84a9541ecae0f9baec65aaf0bea4f23398
Cel <celineregnaud@gmail.com>
2021-03-02 15:32:32 +0100

    connection screen

--- reactapp/src/screens/ConnectionScreen.js
+++ reactapp/src/screens/ConnectionScreen.js
@@ -1,68 +1,58 @@
 import React, {useState} from 'react';
 import '../App.css';
-import { Input, Radio, Button, Form } from 'antd';
+import { Input, Radio, Button} from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 import Nav from '../components/Navbar';
 
 export default function ConnectionScreen() {
-    const [hasAccount, setHasAccount]=useState(false);
-    console.log(hasAccount);
-    const [email, setEmail] = useState();
-    console.log(email);
+    const [ hasAccount, setHasAccount ]=useState(false);
+    const [ email, setEmail ] = useState();
+    const [ emailCheckedFromBack, setEmailCheckedFromBack ] = useState(false);
+    const [ password, setPassword ] = useState();
 
     const checkAccountEmail = async () => {
         if (!checkEmailFormat(email)) {
             console.log('veuillez saisir un email valide.');
-            // pas de redirection
+            // afficher message et pas de redirection
         } else if (!hasAccount && checkEmailFormat(email)) {
             console.log('pas de compte associé à cet email');
-            // rediriger vers sign up.
+            // afficher message et rediriger vers sign up.
         } else if (hasAccount && checkEmailFormat(email)) {
-            console.log('checkemailformat in button', checkEmailFormat(email));
             const response = await fetch('/check-email', {
                 method: 'POST',
                 headers: {'Content-Type':'application/x-www-form-urlencoded'},
                 body: `email=${email}`
             });
             const dataResponse = await response.json();
-            console.log('dataResponse',dataResponse);
+            console.log('dataResponse',dataResponse); // {result:false} ou {result:true}
+            if (dataResponse.result) {
+                setEmailCheckedFromBack(true);
+            }
         } 
     }
-
-    // let handleSubmitSignUp = async (name, email, password) => {
-    //     const response = await fetch('/sign-up', {
-    //       method: 'POST',
-    //       headers: {'Content-Type':'application/x-www-form-urlencoded'},
-    //       body: `usernameFromFront=${name}&emailFromFront=${email}&passwordFromFront=${password}`
-    //     }); 
-    //     const dataResponse = await response.json();
-    //     const userToken = dataResponse.userToken;
-    //     if (userToken) {
-    //       props.addTokenOnSignUpClick(userToken);
-    //     }
-    //     if (dataResponse.result) {    
-    //       setIsLogin(true);
-    //     } else if (dataResponse.message) {
-    //       setMessage(dataResponse.message);
-    //     }
-    //   }
     const checkEmailFormat = (email) => {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return email ? re.test(String(email).toLowerCase()) : false;
     }
 
-    const checkLogin = () => {
-        console.log("click");
+    const checkPasswordToLogin = async () => {
+        const response = await fetch('/log-in', {
+            method: 'POST',
+            headers: {'Content-Type':'application/x-www-form-urlencoded'},
+            body: `email=${email}&password=${password}`
+        });
+        const dataResponse = await response.json();
+        console.log('dataResponse',dataResponse);
     }
 
     return (
         <div>
         <Nav/>
         <div className="container">
-
               <h3>Connexion</h3>
               <p>Connectez-vous pour ajouter des livres à votre bibliothèque et à votre liste d'envies.</p>
-
+            
+            <div className="form">
               <p>Saisissez votre adresse email : </p>
               <Input placeholder="victor@hugo.com" onChange={(e)=>setEmail(e.target.value)} value={email}></Input>
 
@@ -71,14 +61,20 @@ export default function ConnectionScreen() {
                 <Radio style={{display:'block', height:'30px', lineHeight:'30px'}} value={false}>Non, je n'ai pas encore de compte.</Radio>
                 <Radio style={{display:'block', height:'30px', lineHeight:'30px'}} value={true}>Oui, j'ai déjà un compte.</Radio>
              </Radio.Group>
-            <Button onClick={()=> checkAccountEmail()}>Continuer <RightOutlined/></Button>
-
-            {/* A afficher seulement si le mail est bien vérifié */}
-            <p>Saisissez votre mot de passe : </p>
-            <Input placeholder="mot de passe"></Input>
-            <p>mot de passe oublié ?</p>
-            <Button onClick={()=> checkLogin()}>Continuer <RightOutlined/></Button>
-            <p>En vous connectant et en commandant sur notre site, vous acceptez nos Conditions Générales de Vente et notre politique de protection de données personnelles.</p>
+             {(emailCheckedFromBack===false &&
+                <Button onClick={()=> checkAccountEmail()}>Continuer <RightOutlined/></Button>
+              )}
+           
+            {emailCheckedFromBack && 
+            <div>
+                <p>Saisissez votre mot de passe : </p>
+                <Input placeholder="mot de passe" onChange={(e)=> setPassword(e.target.value)}></Input>
+                <p>mot de passe oublié ?</p>
+                <Button onClick={()=> checkPasswordToLogin()}>Continuer <RightOutlined/></Button>
+                <p>En vous connectant et en commandant sur notre site, vous acceptez nos Conditions Générales de Vente et notre politique de protection de données personnelles.</p>
+            </div>
+            }
+            </div>
         </div>
         </div>
       );
