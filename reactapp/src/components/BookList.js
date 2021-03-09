16927736246209e3c261c9bf6e2cb958a2404bde
younes-elgarrai <yelgarrai@gmail.com>
2021-03-09 18:06:06 +0100

    main header

--- reactapp/src/components/BookList.js
+++ reactapp/src/components/BookList.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {Row, Col} from 'antd';
 import AliceCarousel from 'react-alice-carousel';
+import Grid from '@material-ui/core/Grid';
+import Grow from '@material-ui/core/Grow';
 import '../../node_modules/react-alice-carousel/lib/alice-carousel.css';
 import '../App.css'
-import BookCard from '../components/BookCard'
+
+import Skeleton from '@material-ui/lab/Skeleton';
 import Unavailable from '../assets/cover_nondispo.jpg';
 import {Link} from 'react-router-dom';
 
@@ -39,36 +41,46 @@ useEffect(() => {
     const items =  props.data[epoch%size]&&props.data[epoch%size].map((book, index)=>{
         return (
             <div style={styles.bookcard}>
-                <Link to={{pathname:"/book/"+book.id}}><img width="100%" height="100%" src={!book.volumeInfo.imageLinks ? Unavailable : book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}/></Link>
+                <Grow in={true}>
+                    <Link to={{pathname:"/book/"+book.id}}><img width="100%" height="100%" src={!book.volumeInfo.imageLinks ? Unavailable : book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}/></Link>
+                </Grow>
                 {/* <p style={{color:"#333", width:"80%", textAlign:'center', fontSize:"12px", fontWeight:"400", paddingRight:"10px", marginBottom:'10px'}}> {titleCut(book.volumeInfo.title)} </p>  */}
             </div>
             );
     });
 
+    const skeletonArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17].map((num, index)=>{
+        return ( 
+        <div style={styles.bookcard}>
+            <Skeleton variant="rect" width="130px" height="210px" />
+        </div>);
+    });
+
+    var array = props.skeleton?skeletonArray:items;
+    var text = props.skeleton?<Skeleton variant="text" style={{width:"200px"}}/>:<h3 style={styles.h3}>{props.bookListTitle}</h3>;
+
 return (
-<div style={styles.libraryBloc}  className='font'>
-    <Row>
-        <Col xs={24}>
-            <h3 style={styles.h3}>{props.bookListTitle}</h3>
-        </Col>
-    </Row>
-    <Row>
-        <AliceCarousel
-                        mouseTracking
-                        items={items}
-                        fadeOutAnimation={true}
-                        responsive={responsive}
-                        disableButtonsControls={true}
-                    />
-    </Row>
-</div>
+        <Grid container style={styles.libraryBloc}  className='font'>
+                <Grid xs={12}>
+                    {text}
+                </Grid>
+                <Grid xs={12}>
+                    <AliceCarousel
+                                    mouseTracking
+                                    items={array}
+                                    fadeOutAnimation={true}
+                                    responsive={responsive}
+                                    disableButtonsControls={true}
+                                />
+                </Grid>
+        </Grid>
 );
 }
 
 let styles = {
     libraryBloc: {
         justifyContent: 'center',
-        width:'80%',
+        width:'90%',
         backgroundColor: 'white',
         paddingTop:'30px',
         paddingLeft:'30px',
@@ -80,7 +92,7 @@ let styles = {
         justifyContent: "center", 
         marginTop:"10px",
         width: "100%",
-        height: "220px",
+        height: "210px",
     },
 
     h3: {
--- reactapp/src/screens/MainScreen.js
+++ reactapp/src/screens/MainScreen.js
@@ -9,6 +9,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import Skeleton from '@material-ui/lab/Skeleton';
+import Background from '../assets/picto.png'
 
 import '../components/BookHeader'
 import '../App.css';
@@ -16,6 +18,7 @@ import '../App.css';
 import Nav from '../components/Navbar';
 import Review from '../components/Reviews';
 import BookList from '../components/BookList';
+import LibraryHeader from '../components/LibraryHeader';
 
 import subjects from '../assets/subjects';
 
@@ -30,50 +33,7 @@ const useStyles = makeStyles((theme) => ({
       width: '100%',
       color: theme.palette.text.secondary,
     },
-  }));
-  
-
-
-var voidSubj = {'BD & Jeunesse': {
-                    'BD, Comics': [],
-                    'Manga':[],
-                    'Humour' : [],
-                    'Livre jeunesse' : []
-},
-                'Littérature & Fiction': { 
-                    'Roman' : [],
-                    'Poésie & théatre' : [],
-                    'Thriller, Roman Policier' : [],
-                    'Fantasy, Science Fiction' : []
-
-                    },
-                'Vie Pratique':{
-                    'Cuisine': [],
-                    'Self Help' : [], 
-                    'Santé, Bien être' : [], 
-                    'Loisirs Créatifs': []
-                    }, 
-                'Art, Culture & Société':{
-                    'Actualités Politique, Economie, Société' : [], 
-                    'Art, Cinema, Musique': [],
-                    'Biographie, Autobiographie' : [],
-                    'Histoire' : [],
-                    'Religion & Spiritualité' : [],
-                    'Sciences Humaines' : []
-                    }, 
-                'Nature & Loisirs':{
-                    'Nature, Animaux, Jardin' : [],
-                    'Sport, Loisirs, Transport': [],
-                    'Tourisme & Voyage' : [],
-                    }, 
-                'Savoirs':{
-                    'Droit' : [], 
-                    'Entreprise, Management' : [],
-                    'Livres informatique' : [],
-                    'Science & Médecine' : []
-                }};
-
-
+  }));  
 
 var catQueryMaker = (cat, styles)=>{
   
@@ -98,7 +58,6 @@ var queryMaker = (styles) => {
                 r[cats[i]] = queries[i];}
             
             return r; };
-
 
             function TabPanel(props) {
                 const { children, value, index, ...other } = props;
@@ -135,7 +94,6 @@ var queryMaker = (styles) => {
               }
               
                                     
-
 function MainScreen() {
 
 
@@ -149,7 +107,7 @@ function MainScreen() {
 
     const [cookies, setCookie] = useCookies(['survey']);
 
-    const [data, setData] = useState(voidSubj);
+    const [data, setData] = useState(undefined);
 
     var query = cookies.survey!==undefined&&queryMaker(cookies.survey.Styles);
 
@@ -185,23 +143,65 @@ function MainScreen() {
         for (const subcat in data[cat]){
             bloc.push(
             <Grid item xs={12} style={{width:'100%' , display:'flex', justifyContent:'center'}}>
-                <BookList bookListTitle={subcat} data={data[cat][subcat]} />
+                <BookList skeleton={false} bookListTitle={subcat} data={data[cat][subcat]} />
             </Grid>
             )
         }
         suggest.push(
-                <Grid item xs={12}>
+                <Grid item xs={12} className='font'>
                     <h3 style={styles.h3}>{cat}</h3>
                 </Grid>
             )
         suggest.push(bloc);
     }
+
+    function BookSkeleton (){
+        return(
+            <Grid xs={12} container>
+                <Grid item xs={12}>
+                    <Skeleton variant="text" style={{width:"200px"}} />
+                </Grid>
+                <Grid item xs={12} style={{width:'100%' , display:'flex', justifyContent:'center'}}>
+                    <BookList skeleton={true} data={[]}/>
+                </Grid>
+            </Grid>
+        );
+    }
+
      
   return (
 
     <div>
         <Nav />
-        <Grid container spacing={0}   direction="column" justify="center" alignItems="center" style={styles.container}>
+        <Grid container spacing={0}   direction="column" justify="center" alignItems="center" style={styles.container} className='font'>
+            <Grid container xs={12} style={{width:'80%', 
+                                            backgroundColor:'#23396C', 
+                                            borderTopRightRadius:"10px",
+                                            borderTopLeftRadius:"10px", 
+                                            backgroundImage: `url(${Background})`, 
+                                            backgroundSize: '15%',
+                                            backgroundRepeat: 'no-repeat',
+                                        backgroundPosition: 'right bottom',}}>
+                    <Grid container xs={2} direction="column" justify="center" alignItems="center" >
+                        <img style={styles.images} width={120} height={120} src={'http://books.google.com/books/content?id=4fX_DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'} alt={'bookpicture'}/> 
+                    </Grid>
+                    <Grid container xs={4} width="100%" direction="column" justify="center" alignItems="flex-start">
+                        <Grid container xs={6} direction="row" justify="space-between" alignItems="center">
+                            <Grid item>
+                                <h2 style={styles.note}>Livres à lire</h2>
+                            </Grid>
+                            <Grid item>
+                                <h2 style={styles.h2}>XXX</h2>
+                            </Grid>
+                            <Grid item>
+                                <h2 style={styles.note}>Livres dans ma biliothèque</h2>
+                            </Grid>
+                            <Grid item>
+                                <h2 style={styles.h2}>XXX</h2>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+            </Grid>
             <Grid item xs={12} style={{width:'80%'}}>
                 <Paper elevation={0} className={classes.root}>
                         <Tabs
@@ -220,7 +220,7 @@ function MainScreen() {
             <Grid item xs={12} direction="column" justify="center" alignItems="center" style={{width:'80%', backgroundColor:"white"}}>
                 <TabPanel value={value} index={0}>
                         <div style={styles.libraryBloc}>
-                            {suggest}
+                            {data?suggest:<BookSkeleton />}
                         </div> 
                         <Review/> 
                 </TabPanel>
@@ -258,8 +258,41 @@ let styles = {
         fontSize: "16px",
         fontWeight: "500",
         margin: "0px",
-        paddingBottom:"10px"
-      }
+        paddingBottom:"10px",
+        paddingTop:"10px",
+        marginLeft:"50px"
+      },
+
+    images: {
+        borderRadius:100,
+        boxShadow: "1px 1px 1px #e1e1e1",
+        marginTop: "20px",
+        marginBottom: "20px",
+      },
+
+    
+    h1: {
+        color:'white',
+        fontSize: '22px',
+        fontWeight: '700',
+        margin: '0px',
+        paddingTop:'10px',
+        paddingBottom:'10px',
+    },
+
+    h2: {
+        color:'#ffffff',
+        fontSize: '16px',
+        fontWeight: '400',
+        margin: '0px',
+        paddingBottom:'10px',
+    },
+
+    note: {
+        color:'#ffffff',
+        fontSize: '12px',
+        fontWeight: '200',
+    }
 }
 
 export default MainScreen;
